fcedd7e4e02d36acbacba9f673cda699741bf6c3
Alex <alexgrantjnr@yahoo.co.uk>
2018-12-04 20:20:08 +0000

    Almost complete

--- Alex Grant Online CV/js/myJS.js
+++ Alex Grant Online CV/js/myJS.js
@@ -5,3 +5,64 @@ jQuery(document).ready(function(){
 		},6000);
 	});
 });
+
+function openModal() {
+  document.getElementById('myModal').style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
+}
+
+function openModal2() {
+  document.getElementById('myModal2').style.display = "block";
+}
+
+function closeModal2() {
+  document.getElementById('myModal2').style.display = "none";
+}
+
+function openModal3() {
+  document.getElementById('myModal3').style.display = "block";
+}
+
+function closeModal3() {
+  document.getElementById('myModal3').style.display = "none";
+}
+
+function openModal4() {
+  document.getElementById('myModal4').style.display = "block";
+}
+
+function closeModal4() {
+  document.getElementById('myModal4').style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides", "mySlides2", "mySlides3", "mySlides3");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
