dadbe460c63c2b331dad39c7281422c8559262a1
Alex Grant <alexgranyjnr@yahoo.co.uk>
2018-12-09 18:17:28 +0000

    Reference page added

--- js/myJS.js
+++ js/myJS.js
@@ -66,3 +66,7 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
+
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();   
+});
